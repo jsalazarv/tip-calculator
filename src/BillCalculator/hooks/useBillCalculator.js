54b0148ace9abd33dde31d0f4dec7ac42993d747
jsalazarv <jsalazarv8@gmail.com>
2023-01-04 00:10:48 -0600

    It is tested that useBillCalculator returns your calculated data

--- src/BillCalculator/hooks/useBillCalculator.js
+++ src/BillCalculator/hooks/useBillCalculator.js
@@ -9,7 +9,23 @@ export const useBillCalculator = ({
   const [tipAmount, setTipAmount] = useState(0);
   const [total, setTotal] = useState(0);
   const [tipAmountPerPerson, setTipAmountPerPerson] = useState(0);
-  const [totalPerPerson, setTotalPerPerson] = useState(1);
+  const [totalPerPerson, setTotalPerPerson] = useState(0);
+
+  const calculateBill = () => {
+    let tipAmount = (bill * selectedTipPercentage) / 100;
+    let total = bill + tipAmount;
+    let tipAmountPerPerson = tipAmount / selectedQuantityPeople;
+    let totalPerPerson = total / selectedQuantityPeople;
+
+    setTipAmount(tipAmount);
+    setTotal(total);
+    setTipAmountPerPerson(tipAmountPerPerson);
+    setTotalPerPerson(totalPerPerson);
+  };
+
+  useEffect(() => {
+    calculateBill();
+  }, []);
 
   return { bill, tipAmount, total, tipAmountPerPerson, totalPerPerson };
 };
